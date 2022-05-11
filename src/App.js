6033a565883219635ba1a1bcbb5481700d60f358
Lovish Tater <lovishtater@gmail.com>
2022-05-11 15:20:05 +0530

    Added MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant="h1" color="initial">Welcome to groww</Typography>
     </div>
   );
 }
